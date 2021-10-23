51ec994537045ec5efcf8d5523257edd5ca55f70
jyw0154 <jyw2708@gmail.com>
2021-10-24 02:06:31 +0900

    7. 책 상세보기 화면
    [21-10-24][Yongwon, Jeong]
     - 책 상세보기 화면
     - 컨트롤러 '상세'  View 기능 추가

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/90/70637d8e2134001c1d5cd3af144865e3
@@ -0,0 +1,9 @@
+package sample.sprint.yse;
+
+import java.util.Map;
+
+public interface BookService {
+
+	String create(Map<String, Object> map);
+
+}
--- SampleSprintYse/src/main/java/sample/sprint/yse/BookService.java
+++ SampleSprintYse/src/main/java/sample/sprint/yse/BookService.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 public interface BookService {
 
-	String create(Map<String, Object> map);
+	String create(Map<String, Object> map); // 책 데이터 입력 서비스 인터페이스 메소드 시그니처 생성
+
+	Map<String, Object> detail(Map<String, Object> map); // 책 상세 서비스 인터페이스 메소드 시그니처 생성
 
 }
