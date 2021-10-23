3c1d7b84cfab7ac32c113d067734ba0608dacc2e
jyw0154 <jyw2708@gmail.com>
2021-10-24 01:39:19 +0900

    책 데이터 입력 완료
    [21-10-24][Yongwon, Jeong]
     - 컨트롤러 메서드 수정

--- SampleSprintYse/src/main/java/sample/sprint/yse/BookController.java
+++ SampleSprintYse/src/main/java/sample/sprint/yse/BookController.java
@@ -1,17 +1,38 @@
 package sample.sprint.yse;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class BookController {
-
+@Autowired
+BookService bookService;
 	
 	@RequestMapping(value="/create", method = RequestMethod.GET)
 	public ModelAndView create() {
 		return new ModelAndView("book/create");
+	}
+	
+	@RequestMapping(value = "/create", method = RequestMethod.POST)
+	public ModelAndView createPost(@RequestParam Map<String, Object> map) {
+	    ModelAndView mav = new ModelAndView();
+
+	    String bookId = this.bookService.create(map);
+	    // bookID 가 비어있으면 한 번더 입력창을 리다이렉트
+	    if (bookId == null) {
+	        mav.setViewName("redirect:/create");
+	    // bookID가 생성되었으면, 상세페이지로 리다이렉트
+	    }else {
+	        mav.setViewName("redirect:/detail?bookId=" + bookId);	
+	    }		
+
+	    return mav;
 	}
 	
 }
